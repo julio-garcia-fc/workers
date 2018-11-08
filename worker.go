e8de3100b2d0a1e05813b7906087353585d1d486
Garcia, Julio C <jgarcia@digitalocean.com>
2018-11-08 16:27:49 -0700

    Shut up linter

--- worker.go
+++ worker.go
@@ -1,10 +1,12 @@
 package workers
 
+// WG is Worker group
 type WG struct {
 	allDone chan bool
 	main    chan func()
 }
 
+// New creates a new WG
 func New(n int) WG {
 
 	procDone := make(chan bool)
@@ -33,12 +35,13 @@ func New(n int) WG {
 
 }
 
+// Add adds a worker func
 func (wg WG) Add(f func()) {
 	wg.main <- f
 }
 
-//Wait, Call Once when all funcs have been added to wait for completion.
-//If you don't care to wait, 'go Wait' lol
+// Wait Call Once when all funcs have been added to wait for completion.
+// If you don't care to wait, 'go Wait' lol
 func (wg WG) Wait() {
 	close(wg.main)
 	_ = <-wg.allDone
